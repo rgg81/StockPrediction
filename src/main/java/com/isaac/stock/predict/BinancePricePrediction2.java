2adf8965de6507e095e1bc49796c89601003e384
upn1 <roberto.gomes@gmail.com>
2018-04-17 15:25:40 -0300

    go

--- src/main/java/com/isaac/stock/predict/BinancePricePrediction2.java
+++ src/main/java/com/isaac/stock/predict/BinancePricePrediction2.java
@@ -48,9 +48,9 @@ public class BinancePricePrediction2 {
         trainDataUp.reset();
 
         CSVSequenceRecordReader testFeatures = new CSVSequenceRecordReader(1, ",");
-        testFeatures.initialize(new NumberedFileInputSplit(featuresDir.getAbsolutePath() + "/features-clean-%d.csv", 0, 150));
+        testFeatures.initialize(new NumberedFileInputSplit(featuresDir.getAbsolutePath() + "/features-clean-%d.csv", 151, 159));
         CSVSequenceRecordReader testLabels = new CSVSequenceRecordReader(1, ",");
-        testLabels.initialize(new NumberedFileInputSplit(labelsDir.getAbsolutePath() + "/export-BTC-USDT-indicators-label-binance-up-%d.csv", 0, 150));
+        testLabels.initialize(new NumberedFileInputSplit(labelsDir.getAbsolutePath() + "/export-BTC-USDT-indicators-label-binance-up-%d.csv", 151, 159));
         SequenceRecordReaderDataSetIterator testDataIter = new SequenceRecordReaderDataSetIterator(trainFeaturesUp, trainLabelsUp, miniBatchSize, -1, true,
                 SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
 
